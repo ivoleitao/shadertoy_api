e5384f98b6146d36f14545c05c3a8228c01c518f
Ivo Leitão <ivo.leitao@gmail.com>
2020-07-25 11:08:52 +0100

    Added additional tests to increase coverage

--- test/model/input_test.dart
+++ test/model/input_test.dart
@@ -16,6 +16,7 @@ void main() {
       channel: 1,
       sampler: sampler,
       published: 1);
+
   test('Test a input', () {
     expect(input1.id, 'id1');
     expect(input1.src, 'src1');
@@ -29,5 +30,45 @@ void main() {
     var json = input1.toJson();
     var input2 = Input.fromJson(json);
     expect(input1, equals(input2));
+  });
+
+  test('Create a input from a json map', () {
+    var json = {
+      'id': '20250',
+      'src':
+          'https://soundcloud.com/giorgioiannelli/yann_tiersen-amelie_piano_version',
+      'ctype': 'musicstream',
+      'channel': 0,
+      'sampler': {
+        'filter': 'linear',
+        'wrap': 'clamp',
+        'vflip': 'true',
+        'srgb': 'false',
+        'internal': 'byte'
+      },
+      'published': 0
+    };
+
+    expect(() => Input.fromJson(json), returnsNormally);
+  });
+
+  test('Create a input from a json map with a invalid type', () {
+    var json = {
+      'id': '20250',
+      'src':
+          'https://soundcloud.com/giorgioiannelli/yann_tiersen-amelie_piano_version',
+      'ctype': 'xxx',
+      'channel': 0,
+      'sampler': {
+        'filter': 'linear',
+        'wrap': 'clamp',
+        'vflip': 'true',
+        'srgb': 'false',
+        'internal': 'byte'
+      },
+      'published': 0
+    };
+
+    expect(() => Input.fromJson(json), throwsArgumentError);
   });
 }
